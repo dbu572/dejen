28275433d3985b1fcab88b39bd0ec2e39fa1b429
Dejen <dejgeta315@gmail.com>
2025-11-08 16:45:42 +0300

    first_basement

--- Problem_6_santa_floors/first_basement.py
+++ Problem_6_santa_floors/first_basement.py
@@ -1,23 +1,37 @@
 def move_floor(char: str) -> int:
     """
     Returns +1 if '(' else -1 if ')'.
+    Raises ValueError if any other character is found.
     """
-    pass
+    if char == '(':
+        return 1
+    elif char == ')':
+        return -1
+    else:
+        print("ValueErrorInvalid character: only '(' and ')' are allowed.")
 
 
 def first_basement_position(instructions: str) -> int:
     """
-    Returns the position where Santa first enters the basement.
-    Returns -1 if never enters basement.
+    Returns the 1-based position where Santa first enters the basement (floor -1).
+    Returns -1 if Santa never enters the basement.
     """
-    pass
+    floor = 0
+    for i in instructions:
+        floor += move_floor(i)
+        if floor == -1:
+            return floor  # First time reaching the basement
+    
 
 
-# Main program
+# --- Main Program ---
+position=0
 if __name__ == "__main__":
-    instructions = input("Enter Santa's instructions: ").strip()
-    pos = first_basement_position(instructions)
-    if pos == -1:
+    instructions = input("Enter Santa's instructions (only '(' and ')'): ").strip()
+    #strip meances remove white spaces from start and end of string
+    position = first_basement_position(instructions)
+    if position == -1:
         print("Santa never enters the basement.")
     else:
-        print("Santa enters the basement at position:", pos)
+        print("Santa enters the basement at position:", position)
+
